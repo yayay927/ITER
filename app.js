32fe45a1d727db6d1d9430363e4eda00f3936242
Arthur Lin <gn01168178@yahoo.com.tw>
2020-06-05 15:15:23 +0800

    handle page not found

--- app.js
+++ app.js
@@ -34,6 +34,11 @@ app.use('/api/' + API_VERSION,
     ]
 );
 
+// Page not found
+app.use(function(req, res, next) {
+    res.status(404).sendFile(__dirname + '/public/404.html');
+});
+
 // Error handling
 app.use(function(err, req, res, next) {
     console.log(err);
