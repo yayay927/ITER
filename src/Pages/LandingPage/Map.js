7f60db32760a5cd373768e16f972b90378461cb4
Ellie <isabelleya927@gmail.com>
2021-05-13 12:13:15 +0800

    fix react files

--- src/Pages/LandingPage/Map.js
+++ src/Pages/LandingPage/Map.js
@@ -1,12 +1,12 @@
 // Landing Page
-import logo from "./logo.svg";
-import "./App.css";
+// import logo from "./logo.svg";
+// import "./App.css";
 
 function Map() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img className="App-logo" alt="logo" />
         <p>
           Editttttt map <code>src/App.js</code> and save to reload.
         </p>
